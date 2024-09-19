abb0cfe3d65facc97432cc84a24d52f35467fb15
unknown <marmolejo.barajas.arturo>
2024-09-18 20:54:30 -0600

    18/09/2024

---
+++ script.js
@@ -0,0 +1,38 @@
+// 18/09/2024 /Marmolejo Barajas Arturo
+/*
+comentario multi linea jakakka 
+.
+*/
+
+// VARIABLES
+
+/*
+las variables guardan infromacion que puedes
+reutilizar 
+*/
+
+// Declaracion de variables
+
+let nombre = "Arturo"
+const PI = 3.14159;
+var edad = 17;
+
+/*Tipos de datos:
+cadena de texto (string): "hola", 'mundo'
+Numero (number): 5,3,1
+Booleanos (boolean): true, false
+*/
+let mensaje= "Hola mundo";
+let numero = 69;
+let Estudiante = true;
+
+//Operadores matematicos
+let a = 5;
+let b = 2;
+let suma = a+b; // 7
+let resta = a-b; // 3
+let multiplicaicon = a*b; // 10
+let division = a/b; // 2.5
+
+// git add . / git commit -m "18/09/2024"
+// git push
